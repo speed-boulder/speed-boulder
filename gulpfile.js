37c9067b860356a486108c5428af3fb0c32470eb
Grant Freeman <grantfree035@gmail.com>
2016-04-19 00:13:34 -0500

    implemented eslint close #6

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 "use-strict";
 
 const gulp = require('gulp');
-const electronMocha = require('gulp-electron-mocha');
+const eslint = require('gulp-eslint');
+const run = require('gulp-run');
 
-gulp.task('test', => {
-  gulp.src('./app/**.mocha.js', {read: false})
-    .pipe(electronMocha({
-      electronMocha: {
-        renderer:true
-      }
-    }))
+gulp.task('test', ['lint'], () => {
+  run('electron-mocha app').exec();
+  run('electron-mocha app/lib --renderer --recursive').exec();
+});
+
+gulp.task('lint', () => {
+  return gulp.src('./app/**.js')
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 })
 
-gulp.task('watch', => {
+gulp.task('watch', () => {
   gulp.watch('./app/**.js', ['test']);
-})
+});
