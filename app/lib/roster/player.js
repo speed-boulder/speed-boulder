84c99aebd8d8667b1cedc5efd1e1053b382fb021
Grant Freeman <grantfree035@gmail.com>
2016-05-18 21:23:08 -0500

    removed time management from player class

--- app/lib/roster/player.js
+++ app/lib/roster/player.js
@@ -1,41 +1,20 @@
 'use strict'
 
 class Player {
-  // Player Object
-  // This holds most of the information regarding each player.
+  /**
+   * @member {String} name - player's name
+   */
 
-  constructor () {
-    // Constructor
-    // Initialize values with default variables
-    this._name = ''
-    this._times = []
-  }
+  constructor (name) {
+    /**
+     * Initialize Player with given name
+     *
+     * @param {String} name
+     * @return {Player} this
+     */
 
-  name (name) {
-    // Name Getter and Setter
-    // Getter, passes no arguement
-    if (typeof name === 'undefined') return this._name
-    // Setter, name must be a string
-    else if (typeof name === 'string') this._name = name
-    // Error, arg is not a string
-    else console.warn('Player name must be string', name)
-    // Setter and Error cases will return this
-    return this
-  }
-
-  addTime (time) {
-    // Add Time to players record
-    if (typeof time === 'number') this._times.push(time)
-    // throw an error if it is not a number
-    else console.warn('time was not added, must be number', time)
-    // return this for chaining
-    return this
-  }
-
-  times () {
-    // Get Players Times
-    // @return {Array} times
-    return this._times
+    // Initialize name. Use toString() to handle non-string types.
+    this.name = name.toString() || null
   }
 
 }
--- test/roster/player.mocha.js
+++ test/roster/player.mocha.js
@@ -5,72 +5,11 @@ const assert = require('assert')
 const Player = global.require('./lib/roster/player.js')
 
 describe('class Player', () => {
-  describe('#name()', () => {
-    it('should set name when string is passed', () => {
-      let player = new Player()
-      let name = 'john'
-      player.name(name)
-      assert(player._name, name)
-    })
-
-    it('should get name when nothing is passed', () => {
-      let player = new Player()
-      let name = 'john'
-      player._name = name
-      assert(player.name(), name)
-    })
-
-    it('should NOT set name when string is passed', () => {
-      let player = new Player()
-      let types = [null, true, 0, {}]
-      let name = 'john'
-      player._name = name
-      for (let type of types) {
-        player.name(type)
-        assert(player._name, name)
-      }
-    })
-
-    it('should return `this` when name is set', () => {
-      let player = new Player()
-      let name = 'john'
-      assert(player.name(name), player)
-    })
-  })
-
-  describe('#addTime()', () => {
-    it('should add time to player when num is passed', () => {
-      let player = new Player()
-      let time = 1000
-      player.addTime(time)
-      assert(player._times[0], time)
-    })
-
-    it('should NOT add time if not a number', () => {
-      let player = new Player()
-      let types = [null, true, '', {}]
-      let time = 1000
-      player._times.push(time)
-      for (let type of types) {
-        player.addTime(type)
-        assert(player._times.length, player._times.length)
-      }
-    })
-
-    it('should return `this`', () => {
-      let player = new Player()
-      assert(player.addTime(), player)
-    })
-  })
-
-  describe('#times()', () => {
-    it('should return players times', () => {
-      let player = new Player()
-      let time = 1000
-      player._times.push(time)
-      let times = player.times()
-      assert(times.length, 1)
-      assert(times[0], time)
+  describe('#constructor()', () => {
+    it('should set name passed as param.', () => {
+      const NAME = 'john'
+      let PLAYER = new Player(NAME)
+      assert.strictEqual(NAME, PLAYER.name)
     })
   })
 })
